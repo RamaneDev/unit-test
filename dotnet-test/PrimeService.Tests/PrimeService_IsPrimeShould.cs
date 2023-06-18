55dc40af18721e6c8e15949cb3a6f3c70ee88f7b
mesri <abde_mes@yahoo.fr>
2023-06-18 21:33:06 +0200

    using InLineData to test more value

--- dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -13,11 +13,15 @@ namespace Tests
             _primeService = new PrimeService();
         }
 
-        [Fact]
-        public void IsPrime_InputIs1_ReturnFalse()
-        {           
-            bool result = _primeService.IsPrime(1);
-            Assert.False(result, "1 should not be prime");
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            var result = _primeService.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
         }
   
     }
