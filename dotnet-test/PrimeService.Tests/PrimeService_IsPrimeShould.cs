a5ed07c803375233176c7418db12786d6b90a342
mesri <abde_mes@yahoo.fr>
2023-06-18 21:29:09 +0200

    refactoring code using a private PrimeService instance

--- dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ dotnet-test/PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -6,14 +6,19 @@ namespace Tests
 {
     public class PrimeService_IsPrimeShould
     {
-        [Fact]
-        public void IsPrime_InputIs1_ReturnFalse()
+        private readonly PrimeService _primeService;
+
+        public PrimeService_IsPrimeShould()
         {
-            var primeService = new PrimeService();
-            bool result = primeService.IsPrime(1);
-            Assert.False(result, "1 should not be prime");
+            _primeService = new PrimeService();
         }
 
+        [Fact]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {           
+            bool result = _primeService.IsPrime(1);
+            Assert.False(result, "1 should not be prime");
+        }
   
     }
 }
